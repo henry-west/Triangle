9d747c92703e5e81a37ee817c83c6e89e407e1e9
Henry West <henry@henrys-mba.ddns.drake.edu>
2014-08-28 09:37:21 -0500

    Altered to add additional triangle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-//author: Tim Urness
-//date:  August 19, 2014
-//description: This program serves as an intoduction to OpenGL.  Draws a triangle
+//author: Henry West
+//date:  August 27, 2014
+//description: This program serves as an intoduction to OpenGL.  Draws a parallelogram and a triangle
 
 #ifdef __APPLE__
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
@@ -12,22 +12,27 @@
 
 #define WIDTH 500
 #define HEIGHT 400
-
+//extra vertex
 GLfloat vertices[] = {150,100, 350,100, 450,300, 250,300};
+GLfloat extraVertices[] = {200,200, 200,300, 300,250};
 
 void display(void) {
 	/*clear all pixels*/
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	/*set color to red*/
+	/*set color to teal-ish I think -- Alteration 1*/
 	glColor3f(0.1,0.5,0.8);
 
     glEnableClientState(GL_VERTEX_ARRAY);
 
     /*set pointer to vertices array*/
     glVertexPointer(2, GL_FLOAT, 0, vertices);
-    glDrawArrays(GL_POLYGON, 0, 4); //draw the three points
+    glDrawArrays(GL_POLYGON, 0, 4); //draw the four points -- Alteration 2
 
+    glColor3f(1.0, 1.0, 1.0); //set color to white
+
+    glVertexPointer(2, GL_FLOAT, 0, extraVertices);
+    glDrawArrays(GL_POLYGON, 0, 3); //adds extraneous triange -- Alteration 3
     glDisableClientState(GL_VERTEX_ARRAY);  // disable vertex arrays
 
 	/*process buffered OpenGL routines*/
